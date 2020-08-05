33e0bc6a42aef92263b696b93af1a7144a399e5c
Diego Daibert <daibertdiego@gmail.com>
2020-08-04 23:03:12 -0600

    Day 1 - App web.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import './assets/styles/global.css';
+import Routes from './routes';
 
 function App() {
     return (
-        <div className="App">
-            <h1>Teste Funcionando!!!</h1>
-        </div>
+        <Routes/>
     );
 }
 
